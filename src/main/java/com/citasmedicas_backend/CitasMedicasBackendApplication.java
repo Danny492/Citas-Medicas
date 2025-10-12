a8539daf52f0dd3ce9e29f95d1ef1202260ebebc
Danny Henriquez <dh8054302@gmail.com>
2025-10-12 01:08:08 -0400

    base CitasMedicas_Backend project setup

---
+++ src/main/java/com/citasmedicas_backend/CitasMedicasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.citasmedicas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasMedicasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitasMedicasBackendApplication.class, args);
+    }
+
+}
