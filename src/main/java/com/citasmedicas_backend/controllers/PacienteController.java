87027362ec034d6d300cebf754104690eaeff18d
Danny Henriquez <dh8054302@gmail.com>
2025-10-17 10:43:00 -0400

    refactor(paciente):Change the data type, validate endpoint and catch errors (#2)

--- src/main/java/com/citasmedicas_backend/controllers/PacienteController.java
+++ src/main/java/com/citasmedicas_backend/controllers/PacienteController.java
@@ -1,13 +1,21 @@
 package com.citasmedicas_backend.controllers;
 
+import com.citasmedicas_backend.dtos.PacienteDTO;
 import com.citasmedicas_backend.entities.PacienteEntity;
 import com.citasmedicas_backend.services.PacienteService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("api/pacientes")
 public class PacienteController {
@@ -15,29 +23,42 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
+    private static final Logger logger = LoggerFactory.getLogger(PacienteController.class);
+
     @GetMapping
-    public ResponseEntity<List<PacienteEntity>> findAll() {
-        return ResponseEntity.ok(pacienteService.findAll());
+    public ResponseEntity<List<PacienteDTO>> findAll() {
+        return new ResponseEntity<>(pacienteService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PacienteEntity> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(pacienteService.findById(id));
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        PacienteDTO paciente = pacienteService.findById(id);
+        if(paciente == null) {
+            return new ResponseEntity<>(paciente,HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(paciente, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<PacienteEntity> save(@RequestBody PacienteEntity paciente) {
-        return ResponseEntity.ok(pacienteService.save(paciente));
+    public ResponseEntity<?> save(@Valid @RequestBody PacienteDTO paciente, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(pacienteService.save(paciente), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<PacienteEntity> update(@RequestBody PacienteEntity paciente, @PathVariable Long id) {
-        return ResponseEntity.ok(pacienteService.update(paciente, id));
+    public ResponseEntity<?> update(@Valid @RequestBody PacienteDTO paciente, @PathVariable Long id, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(),HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(pacienteService.update(paciente, id), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
         pacienteService.delete(id);
-        return ResponseEntity.ok("Patient deleted");
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 }
