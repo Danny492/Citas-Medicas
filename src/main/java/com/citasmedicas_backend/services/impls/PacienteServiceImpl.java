036b3cf83d11ac1eb83886b06a4b081556f49ae6
Danny Henriquez <dh8054302@gmail.com>
2025-10-17 10:28:39 -0400

    refactor(paciente):Change the data type to dto and add mappers in methods (#2)

--- src/main/java/com/citasmedicas_backend/services/impls/PacienteServiceImpl.java
+++ src/main/java/com/citasmedicas_backend/services/impls/PacienteServiceImpl.java
@@ -1,9 +1,15 @@
 package com.citasmedicas_backend.services.impls;
 
+import com.citasmedicas_backend.controllers.PacienteController;
+import com.citasmedicas_backend.dtos.PacienteDTO;
 import com.citasmedicas_backend.entities.PacienteEntity;
+import com.citasmedicas_backend.mapper.PacienteMapper;
 import com.citasmedicas_backend.repositories.PacienteRepository;
 import com.citasmedicas_backend.services.PacienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,24 +20,29 @@ public class PacienteServiceImpl implements PacienteService {
     @Autowired
     private PacienteRepository pacienteRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(PacienteController.class);
+
     @Override
-    public List<PacienteEntity> findAll() {
-        return pacienteRepository.findAll();
+    public List<PacienteDTO> findAll() {
+        return PacienteMapper.INSTANCE.toDto(pacienteRepository.findAll());
     }
 
     @Override
-    public PacienteEntity findById(Long id) {
-        return pacienteRepository.findById(id).orElse(null);
+    public PacienteDTO findById(Long id) {
+        return PacienteMapper.INSTANCE.toDto(pacienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Paciente not found")));
     }
 
     @Override
-    public PacienteEntity save(PacienteEntity paciente) {
-        return pacienteRepository.save(paciente);
+    public PacienteDTO save(PacienteDTO paciente) {
+        PacienteEntity pacienteEntity = PacienteMapper.INSTANCE.toEntity(paciente);
+        pacienteEntity = pacienteRepository.save(pacienteEntity);
+
+        return PacienteMapper.INSTANCE.toDto(pacienteEntity);
     }
 
     @Override
-    public PacienteEntity update(PacienteEntity paciente, Long id) {
-        PacienteEntity pacienteUpdated = findById(id);
+    public PacienteDTO update(PacienteDTO paciente, Long id) {
+        PacienteDTO pacienteUpdated = findById(id);
         pacienteUpdated.setId(id);
         pacienteUpdated.setNombre(paciente.getNombre());
         pacienteUpdated.setCedula(paciente.getCedula());
