486fa86c07af0626c0f1f88b66219aebecec0871
Danny Henriquez <dh8054302@gmail.com>
2025-10-21 21:36:00 -0400

    refactor(cita):Add DTOs and Validations to controller (#3)

--- src/main/java/com/citasmedicas_backend/controllers/CitaController.java
+++ src/main/java/com/citasmedicas_backend/controllers/CitaController.java
@@ -1,9 +1,12 @@
 package com.citasmedicas_backend.controllers;
 
-import com.citasmedicas_backend.entities.CitaEntity;
+import com.citasmedicas_backend.dtos.CitaDTO;
 import com.citasmedicas_backend.services.impls.CitaServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,28 +19,37 @@ public class CitaController {
     private CitaServiceImpl citaService;
 
     @GetMapping
-    public ResponseEntity<List<CitaEntity>> getCitas(){
-        return ResponseEntity.ok(citaService.citas());
+    public ResponseEntity<List<CitaDTO>> getCitas(){
+        if (citaService.citas().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(citaService.citas(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CitaEntity> getCitaById(@PathVariable Long id){
+    public ResponseEntity<CitaDTO> getCitaById(@PathVariable Long id){
         return ResponseEntity.ok(citaService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<CitaEntity> createCita(@RequestBody CitaEntity cita){
-        return ResponseEntity.ok(citaService.save(cita));
+    public ResponseEntity<?> createCita(@Valid @RequestBody CitaDTO cita, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(),HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(citaService.save(cita), HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<CitaEntity> updateCita(@PathVariable Long id, @RequestBody CitaEntity cita){
-        return ResponseEntity.ok(citaService.update(cita,id));
+    public ResponseEntity<?> updateCita(@Valid@RequestBody CitaDTO cita, BindingResult bindingResult, @PathVariable Long id){
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(citaService.update(cita, id), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteCita(@PathVariable Long id){
         citaService.delete(id);
-        return ResponseEntity.ok("Cita deleted Successfully");
+        return new ResponseEntity<>("Cita deleted Successfully", HttpStatus.OK);
     }
 }
